61028ca19672d2ae8d661aea3b9403868848513c
borkarra <borkarra@gmail.com>
2017-12-02 20:49:53 -0800

    Added feature engineering

--- KKbox.py
+++ KKbox.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd #used to import csv files easily
 import lightgbm as lgb #gradient boosted tree builder
 from sklearn.model_selection import train_test_split #used to easily split data into feature vectors and labels
+from sklearn.metrics import mean_squared_error
 
 data_folder = './data/'
 
@@ -101,6 +102,7 @@ def merge_and_fix_data(train, test, songs, songs_extra, members):
 	test.song_id = test.song_id.astype('category')
 
 	print "Done."
+
 	return train, test, members
 
 # Helper function to convert isrc to a year
@@ -113,7 +115,112 @@ def convert_isrc_to_year(isrc):
 	else:
 		return np.nan
 
+def genre_id_count(x):
+    if x == 'no_genre_id':
+        return 0
+    else:
+        return x.count('|') + 1
 
+def lyricist_count(x):
+    if x == 'no_lyricist':
+        return 0
+    else:
+        return sum(map(x.count, ['|', '/', '\\', ';'])) + 1
+    return sum(map(x.count, ['|', '/', '\\', ';']))
+
+def composer_count(x):
+    if x == 'no_composer':
+        return 0
+    else:
+        return sum(map(x.count, ['|', '/', '\\', ';'])) + 1
+
+def is_featured(x):
+    if 'feat' in str(x) :
+        return 1
+    return 0
+
+def artist_count(x):
+    if x == 'no_artist':
+        return 0
+    else:
+        return x.count('and') + x.count(',') + x.count('feat') + x.count('&')
+
+def song_lang_boolean(x):
+    if '17.0' in str(x) or '45.0' in str(x):
+        return 1
+    return 0
+			
+def add_new_features(train, test):
+	print "Adding new features..."
+	train['genre_ids'].fillna('no_genre_id',inplace=True)
+	test['genre_ids'].fillna('no_genre_id',inplace=True)
+	train['genre_ids_count'] = train['genre_ids'].apply(genre_id_count).astype(np.int8)
+	test['genre_ids_count'] = test['genre_ids'].apply(genre_id_count).astype(np.int8)
+	
+	train['lyricist'].fillna('no_lyricist',inplace=True)
+	test['lyricist'].fillna('no_lyricist',inplace=True)
+	train['lyricists_count'] = train['lyricist'].apply(lyricist_count).astype(np.int8)
+	test['lyricists_count'] = test['lyricist'].apply(lyricist_count).astype(np.int8)
+	
+	train['composer'].fillna('no_composer',inplace=True)
+	test['composer'].fillna('no_composer',inplace=True)
+	train['composer_count'] = train['composer'].apply(composer_count).astype(np.int8)
+	test['composer_count'] = test['composer'].apply(composer_count).astype(np.int8)
+	
+	train['artist_name'].fillna('no_artist',inplace=True)
+	test['artist_name'].fillna('no_artist',inplace=True)
+	train['is_featured'] = train['artist_name'].apply(is_featured).astype(np.int8)
+	test['is_featured'] = test['artist_name'].apply(is_featured).astype(np.int8)
+	
+	train['artist_count'] = train['artist_name'].apply(artist_count).astype(np.int8)
+	test['artist_count'] = test['artist_name'].apply(artist_count).astype(np.int8)
+	
+	train['artist_composer'] = (train['artist_name'] == train['composer']).astype(np.int8)
+	test['artist_composer'] = (test['artist_name'] == test['composer']).astype(np.int8)
+	
+	train['artist_composer_lyricist'] = ((train['artist_name'] == train['composer']) & (train['artist_name'] == train['lyricist']) & (train['composer'] == train['lyricist'])).astype(np.int8)
+	test['artist_composer_lyricist'] = ((test['artist_name'] == test['composer']) & (test['artist_name'] == test['lyricist']) & (test['composer'] == test['lyricist'])).astype(np.int8)
+	
+	train['song_lang_boolean'] = train['language'].apply(song_lang_boolean).astype(np.int8)
+	test['song_lang_boolean'] = test['language'].apply(song_lang_boolean).astype(np.int8)
+	
+	avg_song_length = np.mean(train['song_length'])
+	def smaller_song(x):
+		if x < avg_song_length:
+			return 1
+		return 0
+	train['smaller_song'] = train['song_length'].apply(smaller_song).astype(np.int8)
+	test['smaller_song'] = test['song_length'].apply(smaller_song).astype(np.int8)
+	
+	_dict_count_song_played_train = {k: v for k, v in train['song_id'].value_counts().iteritems()}
+	_dict_count_song_played_test = {k: v for k, v in test['song_id'].value_counts().iteritems()}
+	def count_song_played(x):
+		try:
+			return _dict_count_song_played_train[x]
+		except KeyError:
+			try:
+				return _dict_count_song_played_test[x]
+			except KeyError:
+				return 0
+	train['count_song_played'] = train['song_id'].apply(count_song_played).astype(np.int64)
+	test['count_song_played'] = test['song_id'].apply(count_song_played).astype(np.int64)
+	
+	_dict_count_artist_played_train = {k: v for k, v in train['artist_name'].value_counts().iteritems()}
+	_dict_count_artist_played_test = {k: v for k, v in test['artist_name'].value_counts().iteritems()}
+	def count_artist_played(x):
+		try:
+			return _dict_count_artist_played_train[x]
+		except KeyError:
+			try:
+				return _dict_count_artist_played_test[x]
+			except KeyError:
+				return 0
+	train['count_artist_played'] = train['artist_name'].apply(count_artist_played).astype(np.int64)
+	test['count_artist_played'] = test['artist_name'].apply(count_artist_played).astype(np.int64)
+	
+	print "Done."
+	return train, test
+	
 def train_and_validate(train, test):
 	print "Preparing dev set..."
 	for col in train.columns:
@@ -136,20 +243,25 @@ def train_and_validate(train, test):
 	print "Training model..."
 	lgb_model = lgb.train(params, train_set = lgb_train, valid_sets = lgb_dev, verbose_eval=5)
 	print "Done."
+	y_pred = lgb_model.predict(X_dev)
+	print "dev accuracy: " + str(1 - mean_squared_error(Y_dev, y_pred) ** 0.5)
 	return lgb_model, test
 
 def generate_predictions(lgb_model, test):
 	print "Generating predictions for test set..."
 	X_test = test.drop(['id'], axis=1)
 	ids = test['id'].values
+	print len(ids)
 	predictions = lgb_model.predict(X_test)
+	print len(predictions)
 	output = pd.DataFrame()
-	output['ids'] = ids
+	output['id'] = ids
 	output['target'] = predictions
 	output.to_csv('submission.csv.gz', compression = 'gzip', index=False, float_format = '%.5f')
 
 if __name__ == "__main__":
 	train, test, members, songs, songs_extra = load_data()
 	train, test, members = merge_and_fix_data(train, test, songs, songs_extra, members)
+	train, test = add_new_features(train, test)
 	lgb_model, test = train_and_validate(train, test)
 	generate_predictions(lgb_model, test)
